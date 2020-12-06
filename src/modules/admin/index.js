2568df0d5b64243450f9df3ce99d732a2310e75b
Shakhzodbek Lutfullaevich <Shakhzodbek_lu@ssdsoftwaresolutions.com>
2020-12-06 14:40:30 +0500

    [FEAT] Datahack Unity

--- src/modules/admin/index.js
+++ src/modules/admin/index.js
@@ -3,6 +3,25 @@
 const ajax = require("../../lib/ajax");
 const { $, $$ } = require("../../lib/dom");
 
+async function getProblem(id) {
+    const API_URL = "https://unity-back.herokuapp.com/api/problems/suggestions";
+    const response = await ajax({
+        method: "POST",
+        url: API_URL,
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": localStorage.getItem("token")
+        },
+        body: {
+            "problem_id": id
+        }
+    });
+
+    if (response.status === 200) {
+        return response.data;
+    }
+};
+
 async function problems() {
     const API_URL = "https://unity-back.herokuapp.com/api/problems/all";
     const response = await ajax({
@@ -38,6 +57,7 @@ async function statistics() {
 async function init() {
     console.log(await problems());
     console.log(await statistics());
+    console.log(await getProblem());
 };
 
 window.addEventListener("load", init);
